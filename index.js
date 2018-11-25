e2643d41755674ced18d468ccd31bdc59865aa6d
Ally Netzel <netzelally@gmail.com>
2018-11-25 01:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+  for (const driver of collection) {
+    if (driver === name) {
+      return [driver];
+    }
+  }
+}
