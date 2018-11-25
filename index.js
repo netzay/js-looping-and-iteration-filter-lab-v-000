47ec76a8a53cfa18516b816d3e8582f9bb5e50ec
Ally Netzel <netzelally@gmail.com>
2018-11-25 01:24:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Code your solution in this file
-function findMatching (collection, name) {
-  for (const driver of collection) {
-    if (driver === name) {
-      return [driver];
-    }
-  }
-}
+function findMatching(drivers, name) {
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+};
